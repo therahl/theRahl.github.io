cd24b1a24aeb9d5543a77ffb9112bf5ebc5becf1
Jay Hennessy <JAYHENNESSY@GMAIL.COM>
2015-07-06 12:25:49 -0700

    Automated commit at 2015-07-06 19:25:49 UTC by middleman-deploy 1.0.0

--- javascripts/application.js
+++ javascripts/application.js
@@ -16992,7 +16992,7 @@ $(function() {
     }
   });
 
-	$('.tabs-wrapper').pushpin();
+	$('.tabs-wrapper').pushpin().offset({top: 20});
 	$('.scrollspy').scrollSpy();
 	$('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
@@ -17001,4 +17001,21 @@ $(function() {
       }
     );
     Materialize.showStaggeredList('#nav-mobile');
+
+//Google Analytics
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-64822820-1', 'auto');
+ga('send', 'pageview');
+
 });
